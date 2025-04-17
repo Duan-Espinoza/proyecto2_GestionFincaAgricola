ca734141eaf15d1eda6eadfd1389c147485c0d25
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-04-17 14:09:53 -0600

    Clase trabajador

--- src/Controllers/Trabajadores.hs
+++ src/Controllers/Trabajadores.hs
@@ -1,1 +1,26 @@
-module Controllers.Trabajadores where
+module Controllers.Trabajadores
+    ( validarAcceso
+    , mostrarInformacionTrabajador
+    ) where
+
+import Models.Trabajador
+
+-- Valida si un trabajador existe por cédula
+validarAcceso :: String -> [Trabajador] -> IO (Maybe Trabajador)
+validarAcceso cedulaIngresada lista = do
+    let resultado = buscarTrabajadorPorCedula cedulaIngresada lista
+    case resultado of
+    Just t -> do
+        putStrLn $ "\n Acceso concedido: " ++ nombre t ++ " (" ++ rol t ++ ")"
+        return (Just t)
+    Nothing -> do
+        putStrLn "\n Cédula no registrada. Acceso denegado."
+        return Nothing
+
+-- Mostrar información detallada de un trabajador
+mostrarInformacionTrabajador :: Trabajador -> IO ()
+mostrarInformacionTrabajador t = do
+    putStrLn "\n📋 Información del Trabajador:"
+    putStrLn $ "• Cédula: " ++ cedula t
+    putStrLn $ "• Nombre: " ++ nombre t
+    putStrLn $ "• Rol: " ++ rol t
--- src/Models/Trabajador.hs
+++ src/Models/Trabajador.hs
@@ -1,1 +1,31 @@
-module Models.Trabajador where
+-- Modelo Trabajador 
+-- Atributos: Cedula, Nombre Completo, Rol
+module Models.Trabajador
+  ( Trabajador(..)
+  , trabajadoresIniciales
+  , buscarTrabajadorPorCedula
+  ) where
+
+-- Definición del tipo Trabajador
+data Trabajador = Trabajador
+  { cedula :: String
+  , nombre :: String
+  , rol    :: String
+  } deriving (Show, Eq)
+
+-- Lista inicial de trabajadores registrados
+trabajadoresIniciales :: [Trabajador]
+trabajadoresIniciales =
+  [ Trabajador "101010101" "Geovanni González" "Agrónomo"
+  , Trabajador "202020202" "Antonio Gutierrez" "Supervisor"
+  , Trabajador "303030303" "Moises Duran" "Operario"
+  , Trabajador "404040404" "Dariel Mendez" "Operario"
+  , Trabajador "505050505" "Nehemias Flores" "Supervisor"
+  ]
+
+-- Buscar un trabajador por su cédula
+buscarTrabajadorPorCedula :: String -> [Trabajador] -> Maybe Trabajador
+buscarTrabajadorPorCedula ced lista = 
+  case filter (\t -> cedula t == ced) lista of
+    (x:_) -> Just x
+    []    -> Nothing
