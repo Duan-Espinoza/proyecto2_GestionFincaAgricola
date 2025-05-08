bd6d331e22f24c280a2a32365214b8d93340488a
Geovanni Gonzalez <geovanniga32@gmail.com>
2025-05-08 11:30:11 -0600

    Documentacion interna update

--- src/Models/Cosecha.hs
+++ src/Models/Cosecha.hs
@@ -1,3 +1,48 @@
+{-|
+Módulo      : Models.Cosecha
+Descripción : Este módulo define las estructuras de datos y funciones relacionadas con la gestión de cosechas en una finca agrícola.
+
+Tipos de datos:
+- 'EstadoCosecha': Representa los posibles estados de una cosecha. Puede ser:
+    - 'Planificada': La cosecha está planificada pero aún no ha comenzado.
+    - 'EnCurso': La cosecha está en progreso.
+    - 'Completada': La cosecha ha finalizado.
+    - 'Cancelada': La cosecha ha sido cancelada.
+- 'Cosecha': Representa una cosecha con los siguientes campos:
+    - 'idCosecha': Identificador único de la cosecha.
+    - 'trabajadorId': Identificador del trabajador responsable.
+    - 'parcelaId': Identificador de la parcela donde se realiza la cosecha.
+    - 'fechaInicio': Fecha de inicio de la cosecha.
+    - 'fechaFin': Fecha de finalización de la cosecha.
+    - 'vegetal': Tipo de vegetal cosechado.
+    - 'cantidad': Cantidad cosechada (en unidades específicas).
+    - 'estado': Estado actual de la cosecha ('EstadoCosecha').
+
+Funciones:
+- 'solapamientoFechas': Determina si dos cosechas tienen fechas que se solapan.
+    - Entrada: Dos valores de tipo 'Cosecha'.
+    - Salida: 'True' si las fechas de las cosechas se solapan, 'False' en caso contrario.
+
+- 'parsearFecha': Convierte una cadena de texto en una fecha ('Day') utilizando el formato "YYYY-MM-DD".
+    - Entrada: Una cadena de texto.
+    - Salida: 'Just Day' si la conversión es exitosa, 'Nothing' en caso contrario.
+
+- 'cosechaToCSV': Convierte un registro de tipo 'Cosecha' en una cadena de texto en formato CSV.
+    - Entrada: Un valor de tipo 'Cosecha'.
+    - Salida: Una cadena de texto con los campos separados por comas.
+
+- 'csvToCosecha': Convierte una cadena de texto en formato CSV en un registro de tipo 'Cosecha'.
+    - Entrada: Una cadena de texto en formato CSV.
+    - Salida: 'Just Cosecha' si la conversión es exitosa, 'Nothing' en caso contrario.
+
+- 'split': Divide una cadena de texto en una lista de cadenas utilizando un delimitador específico.
+    - Entrada: Un carácter delimitador y una cadena de texto.
+    - Salida: Una lista de cadenas de texto.
+
+- 'readMaybe': Intenta convertir una cadena de texto en un valor de tipo 'Read'.
+    - Entrada: Una cadena de texto.
+    - Salida: 'Just a' si la conversión es exitosa, 'Nothing' en caso contrario.
+-}
 -- src/Models/Cosecha.hs
 module Models.Cosecha (
     Cosecha(..),
